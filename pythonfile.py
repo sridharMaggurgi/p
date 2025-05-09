95c96e01b8cb368c5d393fea69ff56bb07bf32d8
Sridhar <sridharmaggurgi@gmail.com>
2025-05-09 17:09:23 +0530

    added 3 codes

--- pythonfile.py
+++ pythonfile.py
@@ -1,7 +1,25 @@
-print('hekekeke')
+# 1.	Print numbers from 1 to 10 using a for loop
+from os import remove
 
-a = [1,2,3,5,5]
+for each in range(1,11):
+    print(each)
 
-b = 'abajajakkak'
 
-c = 'sridhar'
+# ============================================================================================
+# 2.	Calculate the sum of numbers from 1 to 10 using a for loop
+
+total = 0
+
+for i in range(1, 11):
+    total += i  # same as total = total + i
+
+print("The sum of numbers from 1 to 10 is:", total)
+
+
+# ==========================================================================================
+
+# 3.	Print the elements of a list using a for loop
+my_list = [1,2,3,4,5,6,7,8,9]
+
+for each in my_list:
+    print(each)
